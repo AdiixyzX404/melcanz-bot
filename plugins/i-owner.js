33ee5a4d6cf396c22efaaee56ba49555bdb747cd
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-12-11 13:25:18 +0800

    Update i-owner.js

--- plugins/i-owner.js
+++ plugins/i-owner.js
@@ -1,6 +1,5 @@
 let handler = async function (m, { conn }) {
   conn.sendContact(m.chat, '60199782326', 'Ownerbot', m)
-  .then(() => m.reply('nih owner gw'))
 }
 handler.help = ['owner']
 handler.tags = ['info']
